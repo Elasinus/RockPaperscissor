36e5d725268afc985f36cee180fc83d2903cb199
Elasinus <ghalepritam2001@gmail.com>
2023-09-16 00:47:03 +0545

    Did all at 1:40 hours

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("hello world");
+const CHOICE = ["ROCK", "PAPER","SCISSOR"];
+let pCOUNT = 0;
+let mCOUNT = 0;
+function getComputerChoice(){
+    return CHOICE[(Math.floor(Math.random() * 3))];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let ps = playerSelection.toUpperCase();
+    let cs = computerSelection.toUpperCase();
+    if (ps === "ROCK" && cs === "SCISSOR") {
+        pCOUNT++;
+        return "You Win! Rock beats Scissor";
+
+    } else if (ps === "PAPER" && cs === "ROCK"){
+        pCOUNT++;
+        return "You Win! PAPER beats ROCK";
+    } else if(ps === "SCISSOR" && cs === "PAPER"){
+        pCOUNT++;
+        return "You Win! SCISSOR beats PAPER";
+    } else if( ps === cs){
+        return "Draw";
+    } else {
+        mCOUNT++;
+        return "You Lose!";
+        
+    }
+}
+
+function main(){
+    for(i=0;i<5;i++){
+        let machine = getComputerChoice();
+        let user = prompt("Enter the option: ");
+        playRound(user,machine);
+    }
+    if (pCOUNT > mCOUNT) {
+        alert("You win!")
+    }else{
+        alert("You Lose!");
+    }
+}
+main();
